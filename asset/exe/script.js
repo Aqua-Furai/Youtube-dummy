d94e367b22278fbbabeb80606410dcfc5d69bdc7
aqua-furai <aqua.31313@gmail.com>
2026-07-29 08:40:47 -0700

    refactor sidebar and footer structure; improve CSS selectors for better maintainability

--- asset/exe/script.js
+++ asset/exe/script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBtn = document.getElementById('menu-toggle');
     const sidebar = document.getElementById('sidebar');
+    const hide = document.getElementById('hide')
 
     if (menuBtn && sidebar) {
         menuBtn.addEventListener('click', () => {
             sidebar.classList.toggle('collapsed');
+            hide.classList.toggle('collapsed');
         });
     }
 });
